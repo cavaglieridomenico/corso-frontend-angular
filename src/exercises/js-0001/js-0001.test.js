ee66ea1179e9cbd95977baac33e417a9c9980768
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-05-16 17:37:37 +0200

    [FEAT] Test functions
    For validation and mathematical operations.

--- src/exercises/js-0001/js-0001.test.js
+++
@@ -1,7 +0,0 @@
-import { getSum } from "./js-0001.js";
-
-test("Sum of two numbers", () => {
-  expect(getSum(1, 2)).toBe(3);
-  expect(getSum(3, 2)).toBe(5);
-  expect(getSum(3, 12)).toBe(15);
-});
