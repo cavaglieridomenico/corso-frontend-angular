7d27e065fd938aa35906a23369a4a2b0c8c0f93f
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-05-18 17:02:14 +0200

    [FEAT] Logic of JS-0012

---
+++ src/exercises/js-0012/js-0012.js
@@ -0,0 +1,13 @@
+import { getBubbleSort } from "./bubble-sorting.js";
+
+/*DOM Elements*/
+const getButton = () => document.querySelector("#button-date");
+const getMessage = () => document.querySelector(".message-to-user-transparent");
+
+const numbers = [90, 77, 150, 34, 3, 80, 9];
+
+getMessage().textContent = numbers;
+
+getButton().addEventListener("click", () => {
+  getMessage().textContent = getBubbleSort(numbers);
+});
