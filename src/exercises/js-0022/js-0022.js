234d2137e6a9562450f772acbd3cbc50030f2deb
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-05-23 14:57:15 +0200

    [FEAT] Logic for JS-0022

---
+++ src/exercises/js-0022/js-0022.js
@@ -0,0 +1,19 @@
+//DOM Elements
+const getResult = () => document.querySelector("#result");
+
+const pc = {};
+
+pc.processor = prompt("Choose your processor");
+pc.hhd = prompt("Choose your hard disk");
+pc.video = prompt("Choose your video card");
+pc.ram = prompt("Choose your RAM");
+
+console.log(pc);
+
+getResult().innerHTML = `
+<h3>Your PC</h3>
+<p><strong>Processor</strong>: ${pc.processor}</p>
+<p><strong>Hard Disk</strong>: ${pc.hhd}</p>
+<p><strong>Video Card</strong>: ${pc.video}</p>
+<p><strong>RAM</strong>: ${pc.ram}</p>
+`;
